f10e3580ddaea6cb705430b594e52c35e154ee17
largechungo <chungusimposter@gmail.com>
2022-03-12 09:48:43 +0000

    Album Page Completion
    Tried making a social media post grabber from scratch but eventually ran into too many problems that would require very impractical solutions so wiped out entire old social media grabber code, and replaced with pre-built social media aggregator, and then wrote code that removes all the branding from the HTML so now everything should work nicely :D

--- docs/album/stuff.js
+++ docs/album/stuff.js
@@ -1,3 +1,14 @@
+//setTimeout(grab,1000);
+//document.getElementById('twitter').contentWindow.location.reload();
+setInterval(unwatermark,100);
+
+function unwatermark(){
+	const chungus = document.getElementsByClassName("referral");
+	const blungus = document.getElementsByClassName("juicer-about");
+	while (chungus.length > 0) chungus[0].remove();
+	while (blungus.length > 0) blungus[0].remove();
+}
+/*
 //Get the button:
 mybutton = document.getElementById("btt");
 
@@ -20,16 +31,39 @@ function topFunction() {
 function grab(){
 	let caption = [];
 	let thumbnail = [];
-	let posts = [];
+	let postText = [];
+	let postImages = [];
 	
 	//Pull information from HAM social media webpages
-	//Use instafeed jquery plugin here
-	//Twitter integration tool of choice TBD
-
+	postText = (document.getElementById('twitter-widget-0').contentWindow.document.getElementsByClassName("timeline-Tweet-text"));
+	//postText.push(document.getElementById('twitter-widget-0').contentWindow.document.getElementsByTagName("p"));
+	postImages = (document.getElementById('twitter-widget-0').contentWindow.document.getElementsByClassName("NaturalImage-image"));
+	
 	//Write to HTML
 	caption = document.getElementsByClassName("card-text");
 	thumbnail = document.getElementsByClassName("bd-placeholder-img card-img-top");
+	console.log(postText);
+	console.log(postImages);
+	console.log(caption);
+	console.log(thumbnail);
+
 	for (let i=0; i<caption.length; i++){
-		caption[i].innerHTML = i.toString();
+		caption[i].innerHTML = "";
 	}
-}
+
+	//write to HTML
+	let ph = [];
+	let img = [];
+	for (let i=0; i<thumbnail.length; i++){
+		ph.push(thumbnail[i].getElementsByTagName("text")[0]);
+		img.push(thumbnail[i].getElementsByTagName("image")[0]);
+		ph[i].innerHTML = "Big Chungus";
+		img[i].href = postImages[i].href;
+		console.log(ph[i])
+			thumbnail.appendChild
+	}
+	for (let i=0; i<caption.length; i++){
+		caption[i].innerHTML = postText[i].innerHTML;
+	}
+
+}*/
