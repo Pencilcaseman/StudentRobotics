cd5cd6b103a16d9189954a35cf21f11dda467eb1
Cy4Shot <veschi@gmail.com>
2022-03-13 16:33:16 +0000

    Update

--- docs/album/stuff.js
+++ docs/album/stuff.js
@@ -6,22 +6,8 @@ function unwatermark() {
   while (referralElements.length > 0) referralElements[0].remove();
   while (juicerElements.length > 0) juicerElements[0].remove();
 }
-
 //Get the button:
 mybutton = document.getElementById("btt");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
