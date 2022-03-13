7ad45ca446755b4a56083ba115e4c241ead6a787
Cy4Shot <veschi@gmail.com>
2022-03-13 15:34:46 +0000

    Website styling :)

--- docs/album/stuff.js
+++ docs/album/stuff.js
@@ -1,19 +1,19 @@
-//setTimeout(grab,1000);
-//document.getElementById('twitter').contentWindow.location.reload();
-setInterval(unwatermark,100);
+setInterval(unwatermark, 100);
 
-function unwatermark(){
-	const chungus = document.getElementsByClassName("referral");
-	const blungus = document.getElementsByClassName("juicer-about");
-	while (chungus.length > 0) chungus[0].remove();
-	while (blungus.length > 0) blungus[0].remove();
+function unwatermark() {
+  const referralElements = document.getElementsByClassName("referral");
+  const juicerElements = document.getElementsByClassName("juicer-about");
+  while (referralElements.length > 0) referralElements[0].remove();
+  while (juicerElements.length > 0) juicerElements[0].remove();
 }
-/*
+
 //Get the button:
 mybutton = document.getElementById("btt");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -28,42 +28,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-function grab(){
-	let caption = [];
-	let thumbnail = [];
-	let postText = [];
-	let postImages = [];
-	
-	//Pull information from HAM social media webpages
-	postText = (document.getElementById('twitter-widget-0').contentWindow.document.getElementsByClassName("timeline-Tweet-text"));
-	//postText.push(document.getElementById('twitter-widget-0').contentWindow.document.getElementsByTagName("p"));
-	postImages = (document.getElementById('twitter-widget-0').contentWindow.document.getElementsByClassName("NaturalImage-image"));
-	
-	//Write to HTML
-	caption = document.getElementsByClassName("card-text");
-	thumbnail = document.getElementsByClassName("bd-placeholder-img card-img-top");
-	console.log(postText);
-	console.log(postImages);
-	console.log(caption);
-	console.log(thumbnail);
-
-	for (let i=0; i<caption.length; i++){
-		caption[i].innerHTML = "";
-	}
-
-	//write to HTML
-	let ph = [];
-	let img = [];
-	for (let i=0; i<thumbnail.length; i++){
-		ph.push(thumbnail[i].getElementsByTagName("text")[0]);
-		img.push(thumbnail[i].getElementsByTagName("image")[0]);
-		ph[i].innerHTML = "Big Chungus";
-		img[i].href = postImages[i].href;
-		console.log(ph[i])
-			thumbnail.appendChild
-	}
-	for (let i=0; i<caption.length; i++){
-		caption[i].innerHTML = postText[i].innerHTML;
-	}
-
-}*/
--- docs/album/stuffGrabber.js
+++
@@ -1,5 +0,0 @@
-caption = document.getElementsByClassName("card-text");
-var i = 0;
-for (i=0;i++;i<caption.length){
-	caption[i].innerHTML = i.toString();
-}
