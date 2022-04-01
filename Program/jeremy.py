e546ffbb466c0970ca155d558f4ddb124e1e3744
ashisnotonline <87771037+ashisnotonline@users.noreply.github.com>
2022-04-01 19:47:41 +0100

    Update jeremy.py

--- Program/jeremy.py
+++ Program/jeremy.py
@@ -1,5 +1,8 @@
 from sr.robot3 import *
 import servo
+import math
+import marker
+import vector
 
 WHEELS = {
 	"fl" : ["SR0WAF", 0],
@@ -82,3 +85,47 @@ class Jeremy:
 
 	def detach(self, servo: str):
 		self.get_servo(servo).detach()
+		
+	def computeRelativePosition(self, marker1, marker2):
+		M1 = Vec3d.marker1.cartesian ##no idea how the vec3d fits in so ive plonked it in there
+		M2 = Vec3d.marker2.cartesian
+		Md = float(sqrt((M2.x - M1.x) * (M2.x - M1.x) + (M2.y - M1.y) * (M2.y - M1.y)))
+		invMd = float(1.0 / Md)
+		D1 = float(M1.mag()) ## dont know what mag is and where to find it so ive just left it in
+		D2 = float(M2.mag())
+		alpha = float(math.atan(M1.y / M1.x) if abs(M1.x) > (1 * (10 **- 5)) else math.pi/2)
+		beta = float(maths.atan(M2.y / M2.x) if abs(M2.x) > (1 * (10 **- 5)) else math.pi/2)
+		
+		if (M1.x < 0):
+			alpha += math.pi
+		if (M2.x < 0):
+			beta += math.pi
+		alpha = math.pi - alpha
+		
+		gamma = float(math.pi - alpha - beta)
+		theta = float(math.asin(D2 * math.sin(gamma)) * invMd)
+		mu = float(math.asin(D1 * math.sin(gamma)) * invMd)
+		
+		worldspaceMarker1 = Marker.m_worldView.idealMarkerPosition(marker1.id) ## dunno if this is where Marker goes
+		worldspaceMarker2 = Marker.m_worldView.idealMarkerPosition(marker2.id)
+		
+		markerDiffWorldSpace = Vec3d(worldspaceMarker2.cartesian - worldspaceMarker1.cartesian) ##lol wtf is Vec3d, i have put it in how i feel like it, vibes n that
+		markerDiffRobotSpace = Vec3d(M2 - M1)
+		tau = float(math.atan2(markerDiffWorldSpace.y, markerDiffWorldSpace.x))
+		kappa = float(math.atan2(markerDiffRobotSpace.y, markerDiffRobotSpace.x))
+		
+		relPosM1 = Vec3d( D1 * cos(theta + tau), D1 * sin(theta + tau) )
+		worldPosM1 = Vec3d(worldspaceMarker1.cartesian + relPosM1)
+		
+		relPosM2 = float( D2 * cos(PI - mu + tau), D2 * sin(PI - mu + tau) )
+		worldPosM2 = float(worldspaceMarker2.cartesian + relPosM2)
+		
+		# "Vec3d worldPosTrue" didnt know how to put this in, its an initiation right?
+		
+		if ((worldspaceMarker1.cartesian - worldPosM1).mag2() >= (worldspaceMarker2.cartesian - worldPosM2).mag2())
+			worldPosTrue = worldPosM1;
+		else
+			worldPosTrue = worldPosM2;
+		
+			##ive gone through this whole bit not knowing hpw to asign variables with custom data types(?)
+			## start from line 757 in other program
