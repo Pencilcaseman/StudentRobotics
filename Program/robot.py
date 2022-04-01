c21d534f5a494369913baa4f0bc2e0f7904b5f7a
Cy4 <veschi@gmail.com>
2022-04-01 10:15:05 +0100

    Robote

--- Program/robot.py
+++ Program/robot.py
@@ -21,12 +21,10 @@ time.sleep(1)
 
 start = time.time()
 while time.time() - start < 30:
-	# jezza.servo(0, ((math.sin(time.time()) + 1) / 2) * 180)
-	# jezza.servo(1, ((math.sin(time.time()) + 1) / 2) * 180)
 
 	angle = ((math.sin(time.time()) + 1) / 2) * 180
-	jezza.grabberServo.setAngle(angle)
-	jezza.armServo.setAngle(angle)
+	jezza.set_angle("grabber", angle)
+	jezza.set_angle("arm", angle)
 
 
 """
